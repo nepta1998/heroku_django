99ee15dd00abc97bfe5d54bc579ef10cbdb717d0
nepta1998 <njpm28@gmail.com>
2020-03-14 23:02:03 -0400

    no se que pasa con esto.

--- api/settings.py
+++ api/settings.py
@@ -128,5 +128,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
