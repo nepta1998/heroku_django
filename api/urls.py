13e811ee4b2c9207d987fbab10671f12d510dc1d
nepta1998 <njpm28@gmail.com>
2020-03-14 22:26:54 -0400

    no se que pasa con esto.

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1.0/api', include('api_message.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
