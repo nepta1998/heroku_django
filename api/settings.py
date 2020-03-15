01bb668b7c0af439f5c4888aeb6d72bc7fe4aa8a
nepta1998 <njpm28@gmail.com>
2020-03-14 23:20:08 -0400

    no se que pasa con esto.

--- api/settings.py
+++ api/settings.py
@@ -130,9 +130,3 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
-}
